d0d6f7d38e622b6abac410ffcb89db09a7e032cd
sherojkarsupriya <sherojkarsupriya@gmail.com>
2023-05-03 17:43:36 +0530

    onetoone

---
+++ src/main/java/com/stumapping/App.java
@@ -0,0 +1,62 @@
+package com.stumapping;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+  
+	public static void main( String[] args )
+    {
+       Configuration con=new Configuration().configure()
+    		   .addAnnotatedClass(student.class).addAnnotatedClass(bike.class);
+       
+        SessionFactory sf=con.buildSessionFactory();
+        
+        Session ses=sf.openSession();
+        Transaction tx=ses.beginTransaction();
+        
+ //       bike b1=new bike();
+ //       b1.setBikeId(101);
+ //       b1.setBikeName("BMW 1250GSA");
+        
+ //       bike b2=new bike();
+ //       b2.setBikeId(102);
+ //       b2.setBikeName("Dominor 400");
+        
+ //       student s1=new student();
+ //       s1.setStId(1);
+ //      s1.setStName("Alex");
+//      s1.setStClass("MCA");
+ //       s1.setBike(b1);
+        
+  //      student s2=new student();
+  //      s2.setStId(2);
+  //      s2.setStName("Hithaksh");
+  //      s2.setStClass("MSC");
+  //      s2.setBike(b2);
+        
+  //      ses.save(b1);
+ //       ses.save(b2);
+//        ses.save(s1);
+//        ses.save(s2);
+        
+      Query qu=ses.createQuery("from student");
+        
+        List<student> ls=qu.getResultList();
+        for(student s:ls)
+        		System.out.println(s.getStName()+" has "+s.getBike().getBikeName());
+            
+        tx.commit();
+    }
+}
---
+++ src/main/java/com/stumapping/student.java
@@ -0,0 +1,58 @@
+package com.stumapping;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+public class student {
+
+	@Id
+	private int stId;
+	private String stName;
+	private String stClass;
+	
+	@OneToOne
+	private bike bike;
+
+	public int getStId() {
+		return stId;
+	}
+
+	public void setStId(int stId) {
+		this.stId = stId;
+	}
+
+	public String getStName() {
+		return stName;
+	}
+
+	public void setStName(String stName) {
+		this.stName = stName;
+	}
+
+	public String getStClass() {
+		return stClass;
+	}
+
+	public void setStClass(String stClass) {
+		this.stClass = stClass;
+	}
+
+	public bike getBike() {
+		return bike;
+	}
+
+	public void setBike(bike bike) {
+		this.bike = bike;
+	}
+
+	@Override
+	public String toString() {
+		return "student [stId=" + stId + ", stName=" + stName + ", stClass=" + stClass + ", bike=" + bike + "]";
+	}
+
+	
+	
+	
+}
